b822373dab902f1e62486e473372f0186e7c7748
Hrithik <slumdog4321@gmail.com>
2023-08-22 23:22:08 +0530

    need figure out to solve the data fetching error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
 import Categories from "@/components/Categories";
 import PostCard from "@/components/PostCard";
 import PostWidget from "@/components/PostWidget";
+import { getPosts } from "@/services";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <main className="container mx-auto px-10 mb-8 text-black ">
+    <main className="container mx-auto px-10 mb-8 ">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1 text-black">
-          <PostCard />
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative gap-8">
@@ -18,4 +21,13 @@ export default function Home() {
       </div>
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  console.log(posts);
+  return {
+    props: { posts },
+  };
 }
