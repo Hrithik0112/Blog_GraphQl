cf53cfdcb8b05e44a8d3dcfa10fa858aebf64324
Hrithik <slumdog4321@gmail.com>
2023-08-27 08:53:28 +0530

    specific article page created and related post

--- src/app/post/[slug]/page.jsx
+++ src/app/post/[slug]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import AdjacentPosts from "@/components/AdjacentPosts";
+// import AdjacentPosts from "@/components/AdjacentPosts";
 import Author from "@/components/Author";
 import Categories from "@/components/Categories";
 import Loader from "@/components/Loader";
@@ -9,19 +9,19 @@ import PostWidget from "@/components/PostWidget";
 import { getPostDetails } from "@/services";
 import { useParams } from "next/navigation";
 
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const PostDetails = () => {
-  // const [postContent, setPostContent] = useState("");
+  const [postContent, setPostContent] = useState("");
 
   const { slug } = useParams();
-  console.log(slug);
+  // console.log(slug);
 
   useEffect(() => {
     getPostDetails(slug).then((result) => {
       console.log(result);
-      // setPostContent(result)
+      setPostContent(result);
     });
   }, [slug]);
 
@@ -30,13 +30,16 @@ const PostDetails = () => {
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
-            <PostDetail />
-            <Author />
-            <AdjacentPosts />
+            <PostDetail post={postContent} />
+            <Author author={postContent.author} />
+            {/* <AdjacentPosts /> */}
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
-              <PostWidget />
+              <PostWidget
+                slug={postContent.slug}
+                categories={postContent?.categories?.map((category) => category.slug)}
+              />
               <Categories />
             </div>
           </div>
